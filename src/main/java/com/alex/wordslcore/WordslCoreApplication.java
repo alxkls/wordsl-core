11335cac6bed9888ad4a4a6f7f620dc36e605a25
Alex <alexey.kolesnichenko11@gmail.com>
2023-10-01 20:32:30 +0100

    init commit

---
+++ src/main/java/com/alex/wordslcore/WordslCoreApplication.java
@@ -0,0 +1,13 @@
+package com.alex.wordslcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordslCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordslCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/wordslcore/WordslCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.wordslcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordslCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

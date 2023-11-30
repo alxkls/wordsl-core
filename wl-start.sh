2fa81830d13527a77712562052647b1c3267f0a3
alxkls <oleksikoleso@gmail.com>
2023-11-30 20:11:00 +0000

    feat: scripts starts app on k8s
    Enhanced start up script to deploy wordsl-core api on local k8s cluster

--- wl-start.sh
+++ wl-start.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
-
 #Author: Alexey020
 #Script to run app
 
+#Deploying postgres on k8s
+kubectl apply -f k8s/postgres/postgres-deploy.yaml
+kubectl apply -f k8s/postgres/postgres-svc.yaml
+
 mvn clean install
-java -jar target/**.jar
+
+kubectl delete deploy wordsl-core-deploy
+docker rmi wordsl-core:1
+
+docker build -t wordsl-core:1 .
+
+#deploying core app on k8s
+kubectl apply -f k8s/wordsl/wordsl-core-deploy.yaml
+kubectl apply -f k8s/wordsl/wordsl-core-svc.yaml
